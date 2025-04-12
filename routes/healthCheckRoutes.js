c8a3bf48a470a479fbb00ab8c5e80accc663c693
Siddhanth <pai.sid@northeastern.edu>
2025-04-11 22:31:52 -0400

    Added /cicd endpoint for CI/CD verification

--- routes/healthCheckRoutes.js
+++ routes/healthCheckRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const healthCheckController = require('../controllers/healthCheckController');
+const cicdCheckController = require('../controllers/cicdCheckController');
+
 
 // Health check route - GET /healthz
 router.get('/healthz', healthCheckController);
@@ -12,5 +14,17 @@ router.delete('/healthz', (req, res) => res.status(405).send());
 router.head('/healthz', (req, res) => res.status(405).send());
 router.options('/healthz', (req, res) => res.status(405).send());
 router.patch('/healthz', (req, res) => res.status(405).send());
+
+// CICD check route - GET /cicd
+router.get('/cicd', cicdCheckController);
+
+// Disallowed methods for /cicd
+router.post('/cicd', (req, res) => res.status(405).send());
+router.put('/cicd', (req, res) => res.status(405).send());
+router.delete('/cicd', (req, res) => res.status(405).send());
+router.head('/cicd', (req, res) => res.status(405).send());
+router.options('/cicd', (req, res) => res.status(405).send());
+router.patch('/cicd', (req, res) => res.status(405).send());
+
 
 module.exports = router;
